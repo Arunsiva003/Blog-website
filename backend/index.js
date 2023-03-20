8b892ddcc47da5465e9662839180087faab50114
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-03-20 17:15:34 +0530

    first comimit

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root123",
+  database: "blog",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM blog.blogs2";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  console.log(req.body);
+  const q = "INSERT INTO blogs2(`title`, `description`, `cover`) VALUES (?)";
+
+  const values = [req.body.title, req.body.description, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const q = "INSERT INTO register(`title`, `description`, `cover`) VALUES (?)";
+
+  const values = [req.body.title, req.body.description, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
